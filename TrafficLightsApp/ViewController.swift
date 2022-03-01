7c1aaacc4596c07ee3b42cbee9ea703b396d29b8
iLexxG <1pismo_na@mail.ru>
2022-03-01 23:33:17 +0300

    Init commit

--- TrafficLightsApp/ViewController.swift
+++ TrafficLightsApp/ViewController.swift
@@ -9,11 +9,46 @@ import UIKit
 
 class ViewController: UIViewController {
 
+    @IBOutlet var redLightView: UIView!
+    @IBOutlet var yellowLightView: UIView!
+    @IBOutlet var greenLightView: UIView!
+    @IBOutlet var switchLightButton: UIButton!
+    @IBOutlet var lightsViews: Array<UIView>!
+    
     override func viewDidLoad() {
         super.viewDidLoad()
-        // Do any additional setup after loading the view.
+        
+        for view in lightsViews {
+            view.layer.cornerRadius = 50
+            view.alpha = 0.3
+        }
     }
-
-
+    
+    @IBAction func switchLightButtonPressed() {
+        let currentLight = (
+            lightsViews.filter {$0.alpha == 1}.isEmpty == false ?
+            lightsViews.filter {$0.alpha == 1} :
+                [greenLightView]
+        )
+        
+        switchLightButton.setTitle("NEXT", for: .normal)
+        
+        switch currentLight[0] {
+        case redLightView:
+            redLightView.alpha = 0.3
+            yellowLightView.alpha = 1
+            
+        case yellowLightView:
+            yellowLightView.alpha = 0.3
+            greenLightView.alpha = 1
+        
+        case greenLightView:
+            greenLightView.alpha = 0.3
+            redLightView.alpha = 1
+            
+        default:
+            break
+        }
+        
+    }
 }
-
